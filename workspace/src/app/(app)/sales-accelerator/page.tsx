3bf101431cfeb9c1d358b67d17edde1580b3a216
Ben Collliss <bencolliss@gmail.com>
2025-06-11 08:42:04 +0000

    starting build "26df5e79-b27b-4644-a338-704e3b20c76f" FETCHSOURCE BUILD

---
+++ workspace/src/app/(app)/sales-accelerator/page.tsx
@@ -0,0 +1,6 @@
+// This file was part of a feature that has been removed.
+// Adding a default export to prevent "not a module" build errors
+// if the file is still picked up by the build process.
+export default function SalesAcceleratorPage() {
+  return null;
+}
