c02ac7a3fd4d65669fb10e8ffdc60e0660b6aa27
Ben Collliss <bencolliss@gmail.com>
2025-06-27 13:31:07 +0000

    The live version is not showing the request headers again, is it because

--- src/components/dashboard/TraderTableClient.tsx
+++ src/components/dashboard/TraderTableClient.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import * as React from "react"; 
@@ -683,3 +684,5 @@ const TooltipContent = React.forwardRef<
   />
 ));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
+
+    
