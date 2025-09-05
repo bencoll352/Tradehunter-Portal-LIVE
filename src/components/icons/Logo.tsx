b0bf6e4d5ca8ff4d0eba3135e513d098ef5ac147
Ben Collliss <bencolliss@gmail.com>
2025-09-05 11:58:30 +0000

    its still not on there - try with these

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,13 +1,11 @@
 
-import type { HTMLAttributes } from 'react';
+import type { SVGProps } from 'react';
 import { cn } from "@/lib/utils";
 import Image from 'next/image';
 
-export function Logo(props: HTMLAttributes<HTMLDivElement>) {
-  const { className, ...rest } = props;
-
+export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
-    <div className={cn("flex items-center justify-center", className)} {...rest}>
+    <div className={cn("flex items-center justify-center", props.className)}>
       <Image 
         src="https://storage.googleapis.com/project-spark-335215.appspot.com/generated/mbbjw4urzzq4/e41q2x7u4bb.png" 
         alt="TradeHunter Pro Logo" 
