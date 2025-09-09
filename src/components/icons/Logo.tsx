b6747f55ab5db13d6417965b541a71ee1ff02021
Ben Collliss <bencolliss@gmail.com>
2025-09-09 16:48:43 +0000

    sort this out now

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -20,12 +20,13 @@ export function Logo({ ...props }: LogoProps) {
     >
       <rect width="250" height="56" fill="white"/>
       <rect width="60" height="56" fill="#1E63C2"/>
-      <g transform="translate(5, 5)">
-        <path d="M129.6 26.4c-0.9-1.8-2-3.6-3.3-5.2C119.9 13.2 113.3 8 105.5 8c-7.8 0-14.4 5.2-20.8 13.2 -1.3 1.6-2.4 3.4-3.3 5.2 -1.4 0.8-1.4 1.8-1.1 2.6 0.4 0.8 1.1 1.4 2 1.7 2.1 0.6 4.3 0.9 6.5 1.1v4.8c0 1.9 1.6 3.5 3.5 3.5h23c1.9 0 3.5-1.6 3.5-3.5v-4.8c2.2-0.2 4.4-0.5 6.5-1.1 0.9-0.3 1.6-0.9 2-1.7C131.6 28.2 131.6 27.2 129.6 26.4z" fill="#1E63C2"/>
-        <circle cx="105.5" cy="21.5" r="9" fill="none" stroke="#FFFFFF" strokeWidth="2.5"/>
-        <line x1="105.5" y1="12.5" x2="105.5" y2="30.5" stroke="#FFFFFF" strokeWidth="2.5"/>
-        <line x1="96.5" y1="21.5" x2="114.5" y2="21.5" stroke="#FFFFFF" strokeWidth="2.5"/>
-        <circle cx="105.5" cy="21.5" r="2" fill="#E53935" stroke="#FFFFFF" strokeWidth="1"/>
+      <g transform="translate(85, 8)">
+        <path d="M38.8571 19.5707C41.7143 19.5707 44 17.3069 44 14.478C44 11.6492 41.7143 9.38538 38.8571 9.38538C36 9.38538 33.7143 11.6492 33.7143 14.478C33.7143 17.3069 36 19.5707 38.8571 19.5707Z" stroke="#1E63C2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M22 40C22 35.8823 25.4286 32.5 29.5714 32.5H48.1429C52.2857 32.5 55.7143 35.8823 55.7143 40V40H22V40Z" stroke="#1E63C2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M5.5 26C5.5 23.2386 7.73858 21 10.5 21H67.5C70.2614 21 72.5 23.2386 72.5 26V32.5H5.5V26Z" fill="#1E63C2" stroke="#1E63C2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M38.8571 14.478V14.478" stroke="white" strokeWidth="3" strokeLinecap="round"/>
+        <circle cx="38.857" cy="14.478" r="3.5" stroke="white" strokeWidth="1.5"/>
+        <circle cx="38.857" cy="14.478" r="1.5" fill="#E53935"/>
       </g>
     </svg>
   );
