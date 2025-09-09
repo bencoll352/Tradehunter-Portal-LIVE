aef4e24746658d35c2f2acaf9eeb6abc6b11af39
Ben Collliss <bencolliss@gmail.com>
2025-09-09 10:32:51 +0000

    i didnt load  - try one with no background

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,4 +1,3 @@
-
 import type { HTMLAttributes } from 'react';
 import { cn } from "@/lib/utils";
 import Image from 'next/image';
@@ -7,7 +6,7 @@ export function Logo(props: HTMLAttributes<HTMLDivElement>) {
   return (
     <div {...props} className={cn("flex items-center justify-center", props.className)}>
       <Image 
-        src="https://storage.googleapis.com/project-spark-335215.appspot.com/generated/mbbjw4urzzq4/4n2k1o32i8h.png" 
+        src="https://storage.googleapis.com/project-spark-335215.appspot.com/generated/mbbjw4urzzq4/4n2l5ksso80.png" 
         alt="TradeHunter Pro Logo" 
         width={150} 
         height={40}
