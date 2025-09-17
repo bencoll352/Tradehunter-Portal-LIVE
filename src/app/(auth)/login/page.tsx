4584e36843ba0d7ba62cb41e325a96de90111b6b
Ben Collliss <bencolliss@gmail.com>
2025-09-17 13:47:01 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,7 @@
-
 import { DynamicLoginFormWrapper } from "@/components/auth/DynamicLoginFormWrapper";
 
 export default function LoginPage() {
     return (
-        <main className="flex items-center justify-center min-h-screen bg-muted/40">
-            <DynamicLoginFormWrapper />
-        </main>
+       <DynamicLoginFormWrapper />
     );
 }
