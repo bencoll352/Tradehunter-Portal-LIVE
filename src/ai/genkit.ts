4601b921f14711d3e405385402e6aa3d71f8c4b7
Ben Collliss <bencolliss@gmail.com>
2025-09-22 11:46:25 +0000

    starting build "aeccb7a7-0e14-49d1-8639-921722b64f2b" FETCHSOURCE BUILD

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,30 +5,29 @@
 
 import { genkit } from 'genkit';
 import { googleAI, gemini15Flash } from '@genkit-ai/googleai';
+import { file } from 'genkit/plugins';
 
 // Initialize Genkit with the Google AI plugin
 export const ai = genkit({
   plugins: [
     googleAI(),
+    // Log all traces to a local file.
+    file({
+      dir: '.genkit',
+      filename: 'traces.jsonl',
+      type: 'trace',
+    }),
+    file({
+      dir: '.genkit',
+      filename: 'flow-state.jsonl',
+      type: 'flowState',
+    }),
   ],
   // Log all traces to the Firebase console.
   // Make sure to set the GCLOUD_PROJECT environment variable.
   // traceStore: {
   //   provider: 'firebase',
   // },
-  // Log all traces to a local file.
-  traceStore: {
-    provider: 'file',
-    options: {
-      path: '.genkit-traces.jsonl',
-    },
-  },
-  flowStateStore: {
-    provider: 'file',
-    options: {
-      path: '.genkit-flow-state.jsonl',
-    }
-  }
 });
 
 export { gemini15Flash };
