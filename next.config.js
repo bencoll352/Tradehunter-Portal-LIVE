daa0e3997fb942bbfbd226ada4a9ca76dc99f9f5
Ben Collliss <bencolliss@gmail.com>
2025-09-24 14:42:13 +0000

    Your commit message

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
@@ -22,8 +21,24 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'streetviewpixels-pa.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.ggpht.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
-
-module.exports = nextConfig;
